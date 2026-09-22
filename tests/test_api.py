f36b76d3a5e2686e04cbf73e8a9034b89ca879fd
kenedyducheine <kmducheine@gmail.com>
2026-09-22 19:55:55 -0400

    added docstrings

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,27 @@
 def test_health_returns_ok(client):
+    '''
+    Verifies the health status code is 200
+
+    Args: 
+    client - Test client used for this assignment
+
+    Returns:
+    {"status" : "ok"}
+    '''
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json()["status"] == "ok"
 
 
 def test_predict_returns_relevant_categories(client, grad_student_request):
+    '''
+    Sending the test grad student's sample categories to the prediction endpoint. Asserts that health endpoint returns 200 and '
+    the categories are either reading, exercies, or exploring. 
+
+    Args: 
+    client - Test client used for this assignment
+    grad_student_requests - grad student's user_id and interests.
+    '''
     response = client.post("/predict", json=grad_student_request.model_dump())
     assert response.status_code == 200
     categories = {r["category"] for r in response.json()["recommendations"]}
@@ -12,6 +29,14 @@ def test_predict_returns_relevant_categories(client, grad_student_request):
 
 
 def test_predict_respects_max_results(client, pentagon_worker_request):
+    '''
+    Sending the test pentagon employee's sample categories to the prediction endpoint. Asserts that health endpoint returns 200 and 
+    the length of the recommendatons is no longer than a specific value (3). 
+
+    Args: 
+    client - Test client used for this assignment
+    pentagon_worker_requests - pentagon employee's user_id and interests.
+    '''
     response = client.post("/predict", json=pentagon_worker_request.model_dump())
     body = response.json()
     assert len(body["recommendations"]) <= pentagon_worker_request.max_results
